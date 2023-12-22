38766fe4c26f9aa4113b0e45b8e553af12d6d1b9
Sudarshan kakde <60603199+sudarshankakde@users.noreply.github.com>
2023-12-22 16:17:22 +0530

    verser speed insights added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import { Outlet } from "react-router-dom";
 import Footer from "./componets/Footer";
 import Loader from ".././src/componets/Loader";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const queryClient = new QueryClient();
 function App() {
   return (
     <div className="">
+      
       <QueryClientProvider client={queryClient}>
         <Loader />
         <Navbar />
 
         <Outlet />
         <Footer />
+        <SpeedInsights />
       </QueryClientProvider>
     </div>
   );
