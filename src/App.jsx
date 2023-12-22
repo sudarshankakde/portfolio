d796578abbaf3c598c0cd7f8fe21f31b33c98c78
Sudarshan kakde <60603199+sudarshankakde@users.noreply.github.com>
2023-12-22 16:22:16 +0530

    role back

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,16 @@ import { Outlet } from "react-router-dom";
 import Footer from "./componets/Footer";
 import Loader from ".././src/componets/Loader";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { SpeedInsights } from "@vercel/speed-insights/react"
 const queryClient = new QueryClient();
 function App() {
   return (
     <div className="">
-      
       <QueryClientProvider client={queryClient}>
         <Loader />
         <Navbar />
 
         <Outlet />
         <Footer />
-        <SpeedInsights />
       </QueryClientProvider>
     </div>
   );
