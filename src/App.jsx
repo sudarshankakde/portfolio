e28c4e9e8d9af8ac1df1926c961a2ef25e13cf5b
Sudarshan kakde <60603199+sudarshankakde@users.noreply.github.com>
2024-03-11 12:59:49 +0530

    auto Scroll to Top

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import { Outlet } from "react-router-dom";
 import Footer from "./componets/Footer";
 import Loader from ".././src/componets/Loader";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import ScrollToTop from "./componets/ScrollToTop";
+
 const queryClient = new QueryClient();
 function App() {
   return (
     <div className="">
       <QueryClientProvider client={queryClient}>
+    <ScrollToTop />
+
         <Loader />
         <Navbar />
         <Outlet />
